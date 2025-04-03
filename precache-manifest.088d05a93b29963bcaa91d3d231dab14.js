a3969e6e2717bf99929db4e799cfa5ed4bbc7e8d
Ting Xu <timtsui@outlook.com>
2025-04-02 22:03:09 -0500

    Updates

---
+++ precache-manifest.088d05a93b29963bcaa91d3d231dab14.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7673fe6ec0f0a5e39bb",
+    "url": "/evonne/static/css/main.90c85aa1.chunk.css"
+  },
+  {
+    "revision": "e7673fe6ec0f0a5e39bb",
+    "url": "/evonne/static/js/main.e7673fe6.chunk.js"
+  },
+  {
+    "revision": "ffba18a0ee3e4f7576dc",
+    "url": "/evonne/static/js/1.ffba18a0.chunk.js"
+  },
+  {
+    "revision": "ca2f2c0f0d26c144ebfe",
+    "url": "/evonne/static/js/runtime~main.ca2f2c0f.js"
+  },
+  {
+    "revision": "c99c024f390f92c56ad93c95670d5bed",
+    "url": "/evonne/static/media/youtube.c99c024f.png"
+  },
+  {
+    "revision": "6983c5ad25c13d2b9aa7c144bbe98529",
+    "url": "/evonne/static/media/evonne-weinhaus-fast-solutiuons.6983c5ad.png"
+  },
+  {
+    "revision": "25dcd0d5ab2302cf93be0a698a78d093",
+    "url": "/evonne/static/media/e-logo.25dcd0d5.png"
+  },
+  {
+    "revision": "763355658d7bcab057af2a7183c9e83d",
+    "url": "/evonne/static/media/evonne-weinhaus-video-0619.76335565.mp4"
+  },
+  {
+    "revision": "26676c6c42e367c791b50690a928b9a8",
+    "url": "/evonne/static/media/evonne-bg.26676c6c.png"
+  },
+  {
+    "revision": "149e9b779708aff693038315dc6ff1e4",
+    "url": "/evonne/static/media/Today_logo.149e9b77.svg"
+  },
+  {
+    "revision": "fcb1a0bb627615ef375020769ab2c33b",
+    "url": "/evonne/static/media/donahue.fcb1a0bb.png"
+  },
+  {
+    "revision": "92820b357690ba546b8fecbd07f789ce",
+    "url": "/evonne/static/media/familycircle.92820b35.png"
+  },
+  {
+    "revision": "39cc3d59aeb8ad38be9a15315311b42e",
+    "url": "/evonne/static/media/godday.39cc3d59.jpg"
+  },
+  {
+    "revision": "afe4c031e9161db14cd4f101d750d589",
+    "url": "/evonne/static/media/goodmorningamerica.afe4c031.png"
+  },
+  {
+    "revision": "4db5f6b479ae488f82570922c3ce6045",
+    "url": "/evonne/static/media/mccalls.4db5f6b4.png"
+  },
+  {
+    "revision": "111a0ddbd0ceae6301322872e6088483",
+    "url": "/evonne/static/media/sally.111a0ddb.webp"
+  },
+  {
+    "revision": "e005bc2f8cf73e83e3ea91c9afac2e97",
+    "url": "/evonne/static/media/womens-day-logo.e005bc2f.png"
+  },
+  {
+    "revision": "2296e460b2404dce1b11fe73868bf7df",
+    "url": "/evonne/static/media/workingmother.2296e460.png"
+  },
+  {
+    "revision": "15b3e3418025a619037a6142be6a598b",
+    "url": "/evonne/static/media/cover.15b3e341.jpg"
+  },
+  {
+    "revision": "7d244d3dc7ffc5ecba69826f00bb98be",
+    "url": "/evonne/static/media/republished-2016.7d244d3d.jpg"
+  },
+  {
+    "revision": "555355406fd5ad4544a501aa2bec197d",
+    "url": "/evonne/static/media/Stars.55535540.svg"
+  },
+  {
+    "revision": "a9db56952bae3195acaf3fa1c1c1bf73",
+    "url": "/evonne/static/media/bg-f.a9db5695.png"
+  },
+  {
+    "revision": "eeb1615ea4e2c48ac3edf70c444b056a",
+    "url": "/evonne/static/media/book-cover.eeb1615e.png"
+  },
+  {
+    "revision": "b1b68eaec4f38ba8b17f42eaebe6bc9e",
+    "url": "/evonne/static/media/evonne-figure.b1b68eae.png"
+  },
+  {
+    "revision": "c30482c0037d1bb0dab25e9c8b6908ed",
+    "url": "/evonne/static/media/Author_BG.c30482c0.png"
+  },
+  {
+    "revision": "3e81c64801deed2b50e9318f9cc08763",
+    "url": "/evonne/static/media/book-magnified.3e81c648.jpg"
+  },
+  {
+    "revision": "87fc64af1d3f4e915661aa0cd34cde9f",
+    "url": "/evonne/static/media/evonne-weinhaus-book-header.87fc64af.jpg"
+  },
+  {
+    "revision": "8d4068d418e1ae678e01d05da159d5b3",
+    "url": "/evonne/static/media/Asset 1Evonne-Logo.8d4068d4.png"
+  },
+  {
+    "revision": "b0ec457f517f29a35a8cf88006b502cc",
+    "url": "/evonne/static/media/envelope.b0ec457f.svg"
+  },
+  {
+    "revision": "23879ee9907facf99e26ab85632c6485",
+    "url": "/evonne/index.html"
+  }
+];
